45e99ddf9a09371ba99bca46aae8fb9bddaa9c3a
Ísis <101017585+isisnakashima@users.noreply.github.com>
2022-11-13 17:55:07 -0300

    Update variaveis_print.py

--- variaveis_print.py
+++ variaveis_print.py
@@ -3,5 +3,5 @@ empresa = "FIAP"
 quantidadeDeFuncionarios = 500
 mediaDeMensalidade = 865.50
 print(nome + " trabalha na empresa " + empresa)
-print("Possui: " ,quantidadeDeFuncionarios, "funcionários")
+print("Possui: ", quantidadeDeFuncionarios, "funcionários")
 print("A média da mensalidade é de: " + str(mediaDeMensalidade))

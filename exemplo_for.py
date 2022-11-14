ef482f79f618bbf4bab8206c5cbf57bfae3425fe
Ísis <101017585+isisnakashima@users.noreply.github.com>
2022-11-14 11:11:15 -0300

    Create exemplo_for.py

---
+++ exemplo_for.py
@@ -0,0 +1,4 @@
+for numero in range(1, int(input("Digite um número para determinar o fim: ")), 1):
+    print("  " + str(numero))
+# o primeiro número determina qual é o número que irá iniciar.
+# o último é a contagem, se vai de 1 em 1. 2 em 2. assim por diante.
